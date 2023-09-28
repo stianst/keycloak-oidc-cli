54034cef9c756f740062493792ca3e1d3f8824d8
stianst <stianst@gmail.com>
2023-09-28 08:49:45 +0200

    Skeleton for testcontainers, not working due to Podman issue

---
+++ src/test/java/org/keycloak/cli/oidc/KeycloakTest.java
@@ -0,0 +1,26 @@
+package org.keycloak.cli.oidc;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+//@QuarkusTestResource(KeycloakTestResource.class)
+// https://github.com/docker-java/docker-java/issues/2201
+// https://github.com/containers/crun/issues/1302
+// Need Podman >=4.6.3?!
+@Disabled
+public class KeycloakTest {
+
+//    @ConfigProperty(name = "keycloak.authServerUrl")
+    private String authServerUrl;
+
+    @Test
+    public void hello() {
+        System.getenv().entrySet().stream().forEach(e -> System.out.println(e.getKey() + " --> " + e.getValue()));
+        System.out.println(authServerUrl);
+    }
+
+}
